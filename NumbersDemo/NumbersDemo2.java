1ddba9e5b6dc3c28f70e4f4db26ea0a2d102badc
Hanna Ingram <hingram@hannas-air.mynetworksettings.com>
2024-10-28 14:54:55 -0400

    updates with reffdiff

--- NumbersDemo/NumbersDemo2.java
+++
@@ -1,9 +0,0 @@
-package NumbersDemo;
-
-public class NumbersDemo2 {
-    public static void displayNumberSquared(Integer number) {
-		Integer numberSquared;
-		numberSquared = number * number; 
-		System.out.println(numberSquared); 
-	}
-}
